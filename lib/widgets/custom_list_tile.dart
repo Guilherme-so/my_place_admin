16464efc193a5e984ea2362cce5a11b964a8e54c
Guilherme <guilhermemagno09@gmail.com>
2023-04-03 21:32:49 -0300

    adding custom ListTile

---
+++ lib/widgets/custom_list_tile.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyCustomListTile extends StatelessWidget {
+  const MyCustomListTile(
+      {this.leading, this.trailing, this.title, this.onTap, super.key});
+
+  final Widget? leading;
+  final Widget? trailing;
+  final Widget? title;
+  final VoidCallback? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      tileColor: Theme.of(context).colorScheme.surface,
+      visualDensity: VisualDensity.compact,
+      leading: leading == null
+          ? null
+          : Container(
+              alignment: Alignment.center,
+              width: 36,
+              child: leading,
+            ),
+      trailing: trailing,
+      title: title,
+      onTap: onTap,
+    );
+  }
+}
